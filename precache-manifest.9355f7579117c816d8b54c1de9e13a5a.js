538662fe4827c4c49012c5e320353dbbfe1d28a7
Abhijeet Kushwaha <abhijeetrkushwaha28@gmail.com>
2020-06-21 21:44:37 +0530

    Updates

---
+++ precache-manifest.9355f7579117c816d8b54c1de9e13a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826f9803837a367deb2b0b132d727a33",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "9a096fc54ecf4ac8a10c",
+    "url": "/Todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8679d9d364252d509a1c",
+    "url": "/Todo/static/js/2.5a8984ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.5a8984ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a096fc54ecf4ac8a10c",
+    "url": "/Todo/static/js/main.0241f0d1.chunk.js"
+  },
+  {
+    "revision": "a7b25b4a47b54fbf8204",
+    "url": "/Todo/static/js/runtime-main.c5e1af5c.js"
+  }
+]);
